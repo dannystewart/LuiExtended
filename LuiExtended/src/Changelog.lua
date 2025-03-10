dfc5e1185b51141158d6f25c0c13411b2488be56
Cullen Daugherty <dakjaniels@outlook.com>
2025-03-10 17:10:20 -0500

    cl vb

--- LuiExtended/src/Changelog.lua
+++ LuiExtended/src/Changelog.lua
@@ -12,6 +12,13 @@ local table_concat = table.concat
 local changelogMessages =
 {
     -- Version Header
+    "|cFFA500LuiExtended Version 6.8.8.2|r",
+    "",
+    -- General Changes
+    "|cFFFF00General:|r",
+    "|t12:12:EsoUI/Art/Miscellaneous/bullet.dds|t Another fix for errors in UnitFrames.lua. Please report any errors in the comments.",
+    "",
+    -- Version Header
     "|cFFA500LuiExtended Version 6.8.8.1|r",
     "",
     -- General Changes
