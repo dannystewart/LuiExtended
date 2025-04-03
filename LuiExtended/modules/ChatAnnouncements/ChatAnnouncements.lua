955a2f967f1f2ae0612cff313fc41a74881b18ca
Cullen Daugherty <dakjaniels@outlook.com>
2025-04-03 14:43:14 -0500

    update collectible api calls

--- LuiExtended/modules/ChatAnnouncements/ChatAnnouncements.lua
+++ LuiExtended/modules/ChatAnnouncements/ChatAnnouncements.lua
@@ -2251,78 +2251,78 @@ end
 -- TODO: Move to a data table outside of CA (maybe?)
 local isShopCollectible =
 {
-    [GetCollectibleInfo(3)] = 3, -- Brown Paint Horse
-    [GetCollectibleInfo(4)] = 4, -- Bay Dun Horse
-    [GetCollectibleInfo(5)] = 5, -- Midnight Steed
+    [GetCollectibleName(3)] = 3, -- Brown Paint Horse
+    [GetCollectibleName(4)] = 4, -- Bay Dun Horse
+    [GetCollectibleName(5)] = 5, -- Midnight Steed
 
-    -- [GetCollectibleInfo(4673)] = 4673, -- Storage Coffer, Fortified (from level up rewards)
-    [GetCollectibleInfo(4674)] = 4674, -- Storage Chest, Fortified (Tel Var / Writ Vouchers)
-    [GetCollectibleInfo(4675)] = 4675, -- Storage Coffer, Oaken (Tel Var / Writ Vouchers)
-    [GetCollectibleInfo(4676)] = 4676, -- Storage Coffer, Secure (Tel Var / Writ Vouchers)
-    [GetCollectibleInfo(4677)] = 4677, -- Storage Coffer, Sturdy (Tel Var / Writ Vouchers)
-    [GetCollectibleInfo(4678)] = 4678, -- Storage Chest, Oaken (Tel Var / Writ Vouchers)
-    [GetCollectibleInfo(4679)] = 4679, -- Storage Chest, Secure (Tel Var / Writ Vouchers)
-    [GetCollectibleInfo(4680)] = 4680, -- Storage Chest, Sturdy (Tel Var / Writ Vouchers)
+    -- [GetCollectibleName(4673)] = 4673, -- Storage Coffer, Fortified (from level up rewards)
+    [GetCollectibleName(4674)] = 4674, -- Storage Chest, Fortified (Tel Var / Writ Vouchers)
+    [GetCollectibleName(4675)] = 4675, -- Storage Coffer, Oaken (Tel Var / Writ Vouchers)
+    [GetCollectibleName(4676)] = 4676, -- Storage Coffer, Secure (Tel Var / Writ Vouchers)
+    [GetCollectibleName(4677)] = 4677, -- Storage Coffer, Sturdy (Tel Var / Writ Vouchers)
+    [GetCollectibleName(4678)] = 4678, -- Storage Chest, Oaken (Tel Var / Writ Vouchers)
+    [GetCollectibleName(4679)] = 4679, -- Storage Chest, Secure (Tel Var / Writ Vouchers)
+    [GetCollectibleName(4680)] = 4680, -- Storage Chest, Sturdy (Tel Var / Writ Vouchers)
 
-    [GetCollectibleInfo(6706)] = 6706, -- Emerald Indrik Feather
-    [GetCollectibleInfo(6707)] = 6707, -- Gilded Indrik Feather
-    [GetCollectibleInfo(6708)] = 6708, -- Onyx Indrik Feather
-    [GetCollectibleInfo(6709)] = 6709, -- Opaline Indrik Feather
+    [GetCollectibleName(6706)] = 6706, -- Emerald Indrik Feather
+    [GetCollectibleName(6707)] = 6707, -- Gilded Indrik Feather
+    [GetCollectibleName(6708)] = 6708, -- Onyx Indrik Feather
+    [GetCollectibleName(6709)] = 6709, -- Opaline Indrik Feather
 
-    [GetCollectibleInfo(6659)] = 6659, -- Dawnwood Berries of Bloom
-    [GetCollectibleInfo(6660)] = 6660, -- Dawnwood Berries of Budding
-    [GetCollectibleInfo(6661)] = 6661, -- Dawnwood Berries of Growth
-    [GetCollectibleInfo(6662)] = 6662, -- Dawnwood Berries of Ripeness
+    [GetCollectibleName(6659)] = 6659, -- Dawnwood Berries of Bloom
+    [GetCollectibleName(6660)] = 6660, -- Dawnwood Berries of Budding
+    [GetCollectibleName(6661)] = 6661, -- Dawnwood Berries of Growth
+    [GetCollectibleName(6662)] = 6662, -- Dawnwood Berries of Ripeness
 
-    [GetCollectibleInfo(6694)] = 6694, -- Luminous Berries of Bloom
-    [GetCollectibleInfo(6695)] = 6695, -- Luminous Berries of Budding
-    [GetCollectibleInfo(6696)] = 6696, -- Luminous Berries of Growth
-    [GetCollectibleInfo(6697)] = 6697, -- Luminous Berries of Ripeness
+    [GetCollectibleName(6694)] = 6694, -- Luminous Berries of Bloom
+    [GetCollectibleName(6695)] = 6695, -- Luminous Berries of Budding
+    [GetCollectibleName(6696)] = 6696, -- Luminous Berries of Growth
+    [GetCollectibleName(6697)] = 6697, -- Luminous Berries of Ripeness
 
-    [GetCollectibleInfo(6698)] = 6698, -- Onyx Berries of Bloom
-    [GetCollectibleInfo(6699)] = 6699, -- Onyx Berries of Budding
-    [GetCollectibleInfo(6700)] = 6700, -- Onyx Berries of Growth
-    [GetCollectibleInfo(6701)] = 6701, -- Onyx Berries of Ripeness
+    [GetCollectibleName(6698)] = 6698, -- Onyx Berries of Bloom
+    [GetCollectibleName(6699)] = 6699, -- Onyx Berries of Budding
+    [GetCollectibleName(6700)] = 6700, -- Onyx Berries of Growth
+    [GetCollectibleName(6701)] = 6701, -- Onyx Berries of Ripeness
 
-    [GetCollectibleInfo(6702)] = 6702, -- Pure-Snow Berries of Bloom
-    [GetCollectibleInfo(6703)] = 6703, -- Pure-Snow Berries of Budding
-    [GetCollectibleInfo(6704)] = 6704, -- Pure-Snow Berries of Growth
-    [GetCollectibleInfo(6705)] = 6705, -- Pure-Snow Berries of Ripeness
+    [GetCollectibleName(6702)] = 6702, -- Pure-Snow Berries of Bloom
+    [GetCollectibleName(6703)] = 6703, -- Pure-Snow Berries of Budding
+    [GetCollectibleName(6704)] = 6704, -- Pure-Snow Berries of Growth
+    [GetCollectibleName(6705)] = 6705, -- Pure-Snow Berries of Ripeness
 
-    [GetCollectibleInfo(7021)] = 7021, -- Spectral Berries of Bloom
-    [GetCollectibleInfo(7022)] = 7022, -- Spectral Berries of Budding
-    [GetCollectibleInfo(7023)] = 7023, -- Spectral Berries of Growth
-    [GetCollectibleInfo(7024)] = 7024, -- Spectral Berries of Ripeness
+    [GetCollectibleName(7021)] = 7021, -- Spectral Berries of Bloom
+    [GetCollectibleName(7022)] = 7022, -- Spectral Berries of Budding
+    [GetCollectibleName(7023)] = 7023, -- Spectral Berries of Growth
+    [GetCollectibleName(7024)] = 7024, -- Spectral Berries of Ripeness
 
-    [GetCollectibleInfo(7791)] = 7791, -- Icebreath Berries of Bloom
-    [GetCollectibleInfo(7792)] = 7792, -- Icebreath Berries of Budding
-    [GetCollectibleInfo(7793)] = 7793, -- Icebreath Berries of Growth
-    [GetCollectibleInfo(7794)] = 7794, -- Icebreath Berries of Ripeness
+    [GetCollectibleName(7791)] = 7791, -- Icebreath Berries of Bloom
+    [GetCollectibleName(7792)] = 7792, -- Icebreath Berries of Budding
+    [GetCollectibleName(7793)] = 7793, -- Icebreath Berries of Growth
+    [GetCollectibleName(7794)] = 7794, -- Icebreath Berries of Ripeness
 
-    [GetCollectibleInfo(8126)] = 8126, -- Mossheart Berries of Bloom
-    [GetCollectibleInfo(8127)] = 8127, -- Mossheart Berries of Budding
-    [GetCollectibleInfo(8128)] = 8128, -- Mossheart Berries of Growth
-    [GetCollectibleInfo(8129)] = 8129, -- Mossheart Berries of Ripeness
+    [GetCollectibleName(8126)] = 8126, -- Mossheart Berries of Bloom
+    [GetCollectibleName(8127)] = 8127, -- Mossheart Berries of Budding
+    [GetCollectibleName(8128)] = 8128, -- Mossheart Berries of Growth
+    [GetCollectibleName(8129)] = 8129, -- Mossheart Berries of Ripeness
 
-    [GetCollectibleInfo(8196)] = 8196, -- Pact Breton Terrier
-    [GetCollectibleInfo(8197)] = 8197, -- Dominion Breton Terrier
-    [GetCollectibleInfo(8198)] = 8198, -- Covenant Breton Terrier
+    [GetCollectibleName(8196)] = 8196, -- Pact Breton Terrier
+    [GetCollectibleName(8197)] = 8197, -- Dominion Breton Terrier
+    [GetCollectibleName(8198)] = 8198, -- Covenant Breton Terrier
 
-    [GetCollectibleInfo(8866)] = 8866, -- Deadlands Flint (Unstable Morpholith)
-    [GetCollectibleInfo(8867)] = 8867, -- Rune-Etched Striker (Unstable Morpholith)
-    [GetCollectibleInfo(8868)] = 8868, -- Smoldering Bloodgrass Tinder (Unstable Morpholith)
+    [GetCollectibleName(8866)] = 8866, -- Deadlands Flint (Unstable Morpholith)
+    [GetCollectibleName(8867)] = 8867, -- Rune-Etched Striker (Unstable Morpholith)
+    [GetCollectibleName(8868)] = 8868, -- Smoldering Bloodgrass Tinder (Unstable Morpholith)
 
-    [GetCollectibleInfo(8869)] = 8869, -- Rune-Scribed Daedra Hide (Deadlands Scorcher)
-    [GetCollectibleInfo(8870)] = 8870, -- Rune-Scribed Daedra Sleeve (Deadlands Scorcher)
-    [GetCollectibleInfo(8871)] = 8871, -- Rune-Scribed Daedra Veil (Deadlands Scorcher)
+    [GetCollectibleName(8869)] = 8869, -- Rune-Scribed Daedra Hide (Deadlands Scorcher)
+    [GetCollectibleName(8870)] = 8870, -- Rune-Scribed Daedra Sleeve (Deadlands Scorcher)
+    [GetCollectibleName(8871)] = 8871, -- Rune-Scribed Daedra Veil (Deadlands Scorcher)
 
-    [GetCollectibleInfo(9085)] = 9085, -- Vial of Simmering Daedric Brew (Deadlands Firewalker)
-    [GetCollectibleInfo(9086)] = 9086, -- Vial of Bubbling Daedric Brew (Deadlands Firewalker)
-    [GetCollectibleInfo(9087)] = 9087, -- Vial of Scalding Daedric Brew (Deadlands Firewalker)
+    [GetCollectibleName(9085)] = 9085, -- Vial of Simmering Daedric Brew (Deadlands Firewalker)
+    [GetCollectibleName(9086)] = 9086, -- Vial of Bubbling Daedric Brew (Deadlands Firewalker)
+    [GetCollectibleName(9087)] = 9087, -- Vial of Scalding Daedric Brew (Deadlands Firewalker)
 
-    [GetCollectibleInfo(9163)] = 9163, -- Black Iron Bit and Bridle (Dagonic Quasigriff)
-    [GetCollectibleInfo(9164)] = 9164, -- Black Iron Stirrups (Dagonic Quasigriff)
-    [GetCollectibleInfo(9162)] = 9162, -- Smoke-Wreathed Griffon Feather (Dagonic Quasigriff)
+    [GetCollectibleName(9163)] = 9163, -- Black Iron Bit and Bridle (Dagonic Quasigriff)
+    [GetCollectibleName(9164)] = 9164, -- Black Iron Stirrups (Dagonic Quasigriff)
+    [GetCollectibleName(9162)] = 9162, -- Smoke-Wreathed Griffon Feather (Dagonic Quasigriff)
 }
 
 function ChatAnnouncements.OnBuyItem(eventCode, itemName, entryType, quantity, money, specialCurrencyType1, specialCurrencyInfo1, specialCurrencyQuantity1, specialCurrencyType2, specialCurrencyInfo2, specialCurrencyQuantity2, itemSoundCategory)
@@ -2331,7 +2331,7 @@ function ChatAnnouncements.OnBuyItem(eventCode, itemName, entryType, quantity, m
         if isShopCollectible[itemName] then
             local id = isShopCollectible[itemName]
             itemName = GetCollectibleLink(id, linkBrackets[ChatAnnouncements.SV.BracketOptionItem])
-            itemIcon = select(3, GetCollectibleInfo(id))
+            itemIcon = GetCollectibleIcon(id)
         else
             itemIcon = GetItemLinkInfo(itemName)
         end
@@ -5417,6 +5417,7 @@ function ChatAnnouncements.InventoryUpdateGuildBank(eventCode, bagId, slotId, is
         g_guildBankCarry.itemLink = itemLink
         g_guildBankCarry.itemId = itemId
         g_guildBankCarry.itemType = itemType
+        g_guildBankCarry.itemQuality = itemQuality
     end
 
     g_itemWasDestroyed = false
@@ -6449,8 +6450,8 @@ function ChatAnnouncements.HookFunction()
         local finalName
         local finalAlertName
 
-        local nameCheck1 = ZO_GetPrimaryPlayerName(displayName, characterName)
-        local nameCheck2 = ZO_GetSecondaryPlayerName(displayName, characterName)
+        local nameCheck1 = ZO_GetPrimaryPlayerName(displayName, characterName, false)
+        local nameCheck2 = ZO_GetSecondaryPlayerName(displayName, characterName, false)
 
         if nameCheck1 == "" then
             finalName = displayName
@@ -6669,8 +6670,8 @@ function ChatAnnouncements.HookFunction()
             LUIE_CallLater(ChatAnnouncements.CheckLFGStatusJoin, 100)
         else
             -- Get character & display names
-            local joinedMemberName = ZO_GetPrimaryPlayerName(displayName, characterName)
-            local joinedMemberAccountName = ZO_GetSecondaryPlayerName(displayName, characterName)
+            local joinedMemberName = ZO_GetPrimaryPlayerName(displayName, characterName, false)
+            local joinedMemberAccountName = ZO_GetSecondaryPlayerName(displayName, characterName, false)
             -- Resolve name links
             local finalName = ChatAnnouncements.ResolveNameLink(joinedMemberName, joinedMemberAccountName)
             local finalAlertName = ChatAnnouncements.ResolveNameNoLink(joinedMemberName, joinedMemberAccountName)
@@ -9959,8 +9960,8 @@ function ChatAnnouncements.HookFunction()
                 local currentTargetCharacterName = self.currentTargetCharacterName
                 local currentTargetCharacterNameRaw = self.currentTargetCharacterNameRaw
                 local currentTargetDisplayName = self.currentTargetDisplayName
-                local primaryName = ZO_GetPrimaryPlayerName(currentTargetDisplayName, currentTargetCharacterName)
-                local primaryNameInternal = ZO_GetPrimaryPlayerName(currentTargetDisplayName, currentTargetCharacterName, USE_INTERNAL_FORMAT)
+                local primaryName = ZO_GetPrimaryPlayerName(currentTargetDisplayName, currentTargetCharacterName, false)
+                local primaryNameInternal = ZO_GetPrimaryPlayerName(currentTargetDisplayName, currentTargetCharacterName, false)
                 local platformIcons = IsInGamepadPreferredMode() and GAMEPAD_INTERACT_ICONS or KEYBOARD_INTERACT_ICONS
                 local ENABLED = true
                 local DISABLED = false
@@ -10923,7 +10924,8 @@ function ChatAnnouncements.AnnounceMemento()
     end
 
     local collectibleId = LUIE.LastMementoUsed
-    local name, _, icon = GetCollectibleInfo(collectibleId)
+    local name = GetCollectibleName(collectibleId)
+    local icon = GetCollectibleIcon(collectibleId)
     local link = GetCollectibleLink(collectibleId, linkBrackets[ChatAnnouncements.SV.BracketOptionCollectibleUse])
 
     local formattedIcon = ChatAnnouncements.SV.Collectibles.CollectibleUseIcon and ("|t16:16:" .. icon .. "|t ") or ""
