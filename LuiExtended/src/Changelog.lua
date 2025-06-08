e040f149562a4cf1efb3f2d93dd793c27f0f54ee
Cullen Daugherty <dakjaniels@outlook.com>
2025-06-07 21:25:55 -0500

    cl

--- LuiExtended/src/Changelog.lua
+++ LuiExtended/src/Changelog.lua
@@ -14,38 +14,21 @@ local GetDisplayName = GetDisplayName
 local changelogMessages =
 {
     -- Version Header
-    "|cFFA500LuiExtended Version 6.9.6.1|r",
+    "|cFFA500LuiExtended Version 7.0.0.0|r",
     "",
     -- Fixes
     "|cFFFF00Fixes:|r",
-    "|t12:12:EsoUI/Art/Miscellaneous/bullet.dds|t Fix for error in gamepad skills menu.",
-    "",
-    -- Version Header
-    "|cFFA500LuiExtended Version 6.9.6.0|r",
+    "|t12:12:EsoUI/Art/Miscellaneous/bullet.dds|t Grim Focus and Bound Armorments count now go to the new 10/8 respectively.",
     "",
     -- New Features
-    "|cFFFF00New Features:|r",
-    "|t12:12:EsoUI/Art/Miscellaneous/bullet.dds|t Unit Frames now display group election information, including current voting status.",
-    "|t12:12:EsoUI/Art/Miscellaneous/bullet.dds|t Added a setting for custom frames to quickly hide dead enemy or neutral NPCs. This shouldn't be hiding dead players, let me know if it does.",
-    "",
-    -- Fixes
-    "|cFFFF00Fixes:|r",
-    "|t12:12:EsoUI/Art/Miscellaneous/bullet.dds|t Addressed an issue where target frames could display duplicate icons. (Note: This fix is still under evaluation and may not fully resolve the issue.)",
-    "|t12:12:EsoUI/Art/Miscellaneous/bullet.dds|t Fixed an issue where the chat announcement system could display incorrect information about restricted communication permissions.",
-    "",
-    -- Improvements
-    "|cFFFF00Improvements:|r",
-    "|t12:12:EsoUI/Art/Miscellaneous/bullet.dds|t Enhanced logic for more accurate identification of Destruction Staff spell icons.",
+    "|cFFFF00New:|r",
+    "|t12:12:EsoUI/Art/Miscellaneous/bullet.dds|t Group buffs/debuffs found in the SpellcastBuff module, this is in beta and can be turned off.",
+    "|t12:12:EsoUI/Art/Miscellaneous/bullet.dds|t Added right click support for buffs so you can easily add/remove them to the prominent buff/debuff and group buff/debuff trackers. work in progress.",
     "",
     -- Miscellaneous
     "|cFFFF00Miscellaneous:|r",
-    "|t12:12:EsoUI/Art/Miscellaneous/bullet.dds|t Started refactoring the codebase to improve readability and maintainability. Started with the Unit Frames module. Report any issues/errors to me in the comments or on Github.",
+    "|t12:12:EsoUI/Art/Miscellaneous/bullet.dds|t I'm sure I missed a note on some other things that changed. View the full change log on Git.",
     "",
-    -- -- Bug Fixes
-    -- "|cFFFF00Bug Fixes:|r",
-    -- "|t12:12:EsoUI/Art/Miscellaneous/bullet.dds|t .",
-    -- "|t12:12:EsoUI/Art/Miscellaneous/bullet.dds|t .",
-    -- "",
 }
 -- -----------------------------------------------------------------------------
 -- Hide toggle called by the menu or xml button
